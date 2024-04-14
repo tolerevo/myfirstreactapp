352d1ae41035331a89ea492ed53d4a5fe9cbe340
tolerevo <tolerevo@hotmail.com>
2024-04-14 14:36:45 -0700

    first commit add website link and change logo with my face :)

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://avatars.githubusercontent.com/u/114973390?v=4" className="App-logo" alt="logo" />
+        <h1><en>To gorgeous to function</en></h1>
+        <a
+          className="App-link"
+          href="https://www.oftatole.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn me! <en>Diego Tolentino the Best!</en>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
